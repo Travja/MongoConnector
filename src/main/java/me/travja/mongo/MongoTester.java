1142c2219a606ebd929d7ae72c4487066200c1c5
Cole Damon <coledamon2001@gmail.com>
2020-07-17 16:03:16 -0600

    Cleanup of Create

--- src/main/java/me/travja/mongo/MongoTester.java
+++ src/main/java/me/travja/mongo/MongoTester.java
@@ -33,8 +33,15 @@ public class MongoTester {
     private Menu menu = new Menu("What would you like to do?",
             new MenuOption("Create", () -> {
                 String[] fields = {"person.first_name", "person.last_name", "person.gender", "person.nick_name","cell_phone_number", "pet", "address.street","address.city","address.state","address.zip_code","home.email","home.phone_number","work.email","work.phone_number","skill","car.year","car.make","car.model"};
-                ArrayList<Bson> docs = new ArrayList<>();
-                Document doc = new Document(), person = new Document(), address = new Document(), home = new Document(), work = new Document(), car = new Document();
+                ArrayList<String> nonNulls = new ArrayList<>(Arrays.asList("person.first_name", "person.last_name", "person.gender", "address.street", "address.city", "address.state", "address.zip_code"));
+                Document doc = new Document();
+                doc.append("person", new Document().append("first_name", "").append("last_name","").append("nick_name", ""))
+                        .append("cell_phone_number", "").append("pet", new String[0])
+                        .append("address", new Document().append("street","").append("city","").append("state","").append("zip_code",""))
+                        .append("home", new Document().append("email","").append("phone_number",""))
+                        .append("work", new Document().append("email","").append("phone_number",""))
+                        .append("skill", new String[0])
+                        .append("car", new Document().append("year",0).append("make","").append("model",""));
 
                 boolean carError;
                 for(String s : fields) {
@@ -47,19 +54,24 @@ public class MongoTester {
                         }
                         value = IOUtils.promptForString("Enter a value for the field \"" + s + "\" (enter null to not fill):");
                         it++;
-                        if(s.equals("car.year") && !value.equals("null")) {
-                            try {
-                                Integer.parseInt(value);
-                                car.put(s.substring(s.indexOf(".")+1), Integer.parseInt(value));
-                            } catch(NumberFormatException nfe) {
-                                carError = true;
-                                System.out.println("The car.year field must have a valid year entered");
+                        if(s.equals("car.year")) {
+                            if(value.toLowerCase().equals("null")) {
+                                ((Document)doc.get("car")).remove("year");
+                            }
+                            else {
+                                try {
+                                    Integer.parseInt(value);
+                                    ((Document) doc.get("car")).put("year", Integer.parseInt(value));
+                                } catch (NumberFormatException nfe) {
+                                    carError = true;
+                                    System.out.println("The car.year field must have a valid year entered");
+                                }
                             }
                         }
                         else if(s.equals("person.gender")) {
                             value = value.toUpperCase();
                         }
-                    } while (carError || (value.toLowerCase().equals("null") && (s.equals("person.first_name") || s.equals("person.last_name") || s.equals("person.gender") || s.equals("address.street") || s.equals("address.city") || s.equals("address.state") || s.equals("address.zip_code"))) || (s.equals("person.gender") && !(value.equals("M") || value.equals("F"))));
+                    } while (carError || (value.toLowerCase().equals("null") && nonNulls.contains(s)) || (s.equals("person.gender") && !(value.equals("M") || value.equals("F"))));
                     if(!value.equals("null") && (s.equals("pet") || s.equals("skill")) ) {
                         ArrayList<String> values = new ArrayList<>();
                         values.add(value);
@@ -67,48 +79,26 @@ public class MongoTester {
                             values.add(IOUtils.promptForString("Enter a value for the field \"" + s + "\":"));
                         }
                         String[] arr = values.toArray(new String[0]);
-                        doc.put(s, Arrays.asList(arr));
+                        doc.replace(s, Arrays.asList(arr));
                     }
-                    else if(!value.equals("null") && !s.equals("car.year")) {
-                        switch(s.substring(0, s.contains(".") ? s.indexOf(".") : s.length())) {
-                            case "person":
-                                person.put(s.substring(s.indexOf(".")+1), value);
-                                break;
-                            case "address":
-                                address.put(s.substring(s.indexOf(".")+1), value);
-                                break;
-                            case "home":
-                                home.put(s.substring(s.indexOf(".")+1), value);
-                                break;
-                            case "work":
-                                work.put(s.substring(s.indexOf(".")+1), value);
-                                break;
-                            case "car":
-                                car.put(s.substring(s.indexOf(".")+1), value);
-                                break;
-                            default:
-                                doc.put(s, value);
+                    else if(!s.equals("car.year")) {
+                        int endIndex = s.contains(".") ? s.indexOf(".") : s.length();
+                        if(value.toLowerCase().equals("null")) {
+                            if(s.contains(".")) {
+                                ((Document) doc.get(s.substring(0, endIndex))).remove(s.substring(s.indexOf(".")+1));
+                            }
+                            else {
+                                doc.remove(s);
+                            }
+                        }
+                        else if(s.contains(".")) {
+                            ((Document) doc.get(s.substring(0, endIndex))).replace(s.substring(s.indexOf(".")+1), value);
+                        }
+                        else {
+                            doc.replace(s, value);
                         }
                     }
-                    switch (s) {
-                        case "person.nick_name":
-                            doc.put("person", person);
-                            break;
-                        case "address.zip_code":
-                            doc.put("address", address);
-                            break;
-                        case "home.phone_number":
-                            doc.put("home", home);
-                            break;
-                        case "work.phone_number":
-                            doc.put("work", work);
-                            break;
-                        case "car.model":
-                            doc.put("car", car);
-                            break;
-                    }
                 }
-                System.out.println(doc.toString());
                 collection.insertOne(doc);
             }),
             new MenuOption("Search (Read)", () -> {
